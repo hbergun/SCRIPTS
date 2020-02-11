0382b699c312ce56c99e26362ced98fab75ff722
Berk Ergün <hasan.berk.ergun@hotmail.com>
2020-02-11 10:20:18 +0300

    IISLogWriter.ps1 Created

---
+++ IISLogWriter.ps1
@@ -0,0 +1,4 @@
+﻿$LogFilePath = 'E:\D-Teknoloji\Powershell Script\u_ex200211.log'
+#$Fields = (Get-Content -Path $LogFilePath -TotalCount 4)[-1] -split ' '
+$Fields = (Get-Content -Path $LogFilePath)
+echo($Fields)
